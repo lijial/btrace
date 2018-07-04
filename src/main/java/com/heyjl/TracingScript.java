5608af44ab48b5ac59ad38d258008967a48ac585
李家良 <jialiang.li@tongdun.cn>
2018-07-04 15:11:40 +0800

    加上cost 耗时监控

--- src/main/java/com/heyjl/TracingScript.java
+++ src/main/java/com/heyjl/TracingScript.java
@@ -6,8 +6,11 @@ package com.heyjl;
  * jps CaseObjectMain的pid
  * btrace pid btrace/src/main/java/com/heyjl/TracingScript.java
  *
+ * 精品：https://www.jianshu.com/p/dbb3a8b5c92f
+ *
  * Created by jalo on 2017/9/22.
  */
+import com.sun.btrace.BTraceUtils;
 import com.sun.btrace.annotations.*;
 import static com.sun.btrace.BTraceUtils.*;
 
@@ -18,9 +21,10 @@ public class TracingScript {
     @OnMethod(clazz="com.heyjl.CaseObject",method="execute",location=@Location(Kind.RETURN))
     /*主要两个参数是对象自己的引用 和 返回值，其它参数都是方法调用时传入的参数*/
     public static void traceExecute(@ProbeClassName String probeClassName,@ProbeMethodName String probeMethodName,
-                                    int sleepTime, @Return boolean result){
+                                    int sleepTime, @Return boolean result,@Duration long time){
         println("调用堆栈！！");
         jstack();
+        BTraceUtils.println("cost time:" + time);
         println(strcat("被测类名：",str(probeClassName)));
         println(strcat("被测函数名：",str(probeMethodName)));
         println(strcat("被测函数入参是：",str(sleepTime)));
